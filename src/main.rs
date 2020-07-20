03b65879237267ef1a45c8b506be900586bb59d0
Jack Parsons <jcksnparsons@yahoo.com>
2020-07-20 12:41:47 -0500

    starting over with different solution

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,20 @@
 fn main() {
-    const CHRISTMAS_CAROL_LYRICS: [&str; 12] = ["a patridge in a pear tree", "two turtle doves", "three french hens", "four calling birds", "FIVE GOLDEN RINGS!", "six geese a-laying", "seven swans a-swimming", "eight mades a milking", "nine ladies dancing", "ten lords-a leaping", "eleven pipers piping", "twelve drummers drumming"];
+    let christmas_carol_lyrics: [&str; 12] = [
+        "a patridge in a pear tree",
+        "two turtle doves",
+        "three french hens",
+        "four calling birds",
+        "FIVE GOLDEN RINGS!",
+        "six geese a-laying",
+        "seven swans a-swimming",
+        "eight mades a milking",
+        "nine ladies dancing",
+        "ten lords-a leaping",
+        "eleven pipers piping",
+        "twelve drummers drumming",
+    ];
 
-    for i in 0..CHRISTMAS_CAROL_LYRICS.len() {
-        println!("On the 12th day of christmas my true love gave to me:");
-        println!("{}", CHRISTMAS_CAROL_LYRICS[i]);
+    for line in christmas_carol_lyrics.iter() {
+        println!("{}", line)
     }
 }
